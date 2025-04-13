dd79e2704eff8dfcacc226fe0bc352bcf5bcf2a2
Iryna <123470957+irinahnatenko@users.noreply.github.com>
2025-04-13 12:32:29 +0300

    Update main.py

--- lec02/main.py
+++ lec02/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from job.fetch_sales import fetch_sales_data
-import os
 
 app = Flask(__name__)
 
@@ -9,11 +8,14 @@ def run_job():
     data = request.get_json()
     page = data.get('page')
     date = data.get('date')
+    raw_dir = data.get('raw_dir')
 
-    if not raw_dir or not date:
-        return jsonify({"error": "Missing raw_dir or date parameter"}), 400
+    if not raw_dir or not date or not page:
+        return jsonify({"error": "Missing raw_dir, page or date parameter"}), 400
 
-    fetch_sales_data(page, date)
+    if fetch_sales_data(page, date, raw_dir) is None:
+        return jsonify({"error": "Failed to fetch sales data"}), 500
+
     return jsonify({"status": "success"}), 200
 
 if __name__ == '__main__':
