8214a9f65c8bb90d5e681f59708cb21c01165874
Iryna <123470957+irinahnatenko@users.noreply.github.com>
2025-04-13 11:40:53 +0300

    Update main.py

--- lec02/main.py
+++ lec02/main.py
@@ -1,20 +1,51 @@
-from flask import Flask, request, jsonify
-from job.fetch_sales import fetch_sales_data
+"""
+This file contains the controller that accepts command via HTTP
+and trigger business logic layer
+"""
 import os
+from flask import Flask, request
+from flask import typing as flask_typing
+
+from Lecture_2.ht_template.job1.bll.sales_api import save_sales_to_local_disk
+
+
+AUTH_TOKEN = os.environ.get("API_AUTH_TOKEN")
+
+if not AUTH_TOKEN:
+    print("AUTH_TOKEN environment variable must be set")
+
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['POST'])
-def run_job():
-    data = request.get_json()
-    raw_dir = data.get('raw_dir')
-    date = data.get('date')
+def main() -> flask_typing.ResponseReturnValue:
+    """
+    Controller that accepts command via HTTP and
+    trigger business logic layer
 
-    if not raw_dir or not date:
-        return jsonify({"error": "Missing raw_dir or date parameter"}), 400
+    Proposed POST body in JSON:
+    {
+      "data: "2022-08-09",
+      "raw_dir": "/path/to/my_dir/raw/sales/2022-08-09"
+    }
+    """
+    input_data: dict = request.json
+    # TODO: implement me
+    date = input_data.get('date')
+    raw_dir = input_data.get('raw_dir')
 
-    fetch_sales_data(raw_dir, date)
-    return jsonify({"status": "success"}), 200
+    if not date:
+        return {
+            "message": "date parameter missed",
+        }, 400
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)
+    save_sales_to_local_disk(date=date, raw_dir=raw_dir)
+
+    return {
+               "message": "Data retrieved successfully from API",
+           }, 201
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost", port=8081)
