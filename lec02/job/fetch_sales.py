f0875b4a1fef96b739929357d06ad71e6c6a4035
Iryna <123470957+irinahnatenko@users.noreply.github.com>
2025-04-13 12:30:01 +0300

    Update fetch_sales.py

--- lec02/job/fetch_sales.py
+++ lec02/job/fetch_sales.py
@@ -3,21 +3,21 @@ import requests
 import json
 from dotenv import load_dotenv
 
-def fetch_sales_data(raw_dir: str, date: str) -> None:
+def fetch_sales_data(page: str, date: str) -> None:
     """
     Fetch sales data from external API and save it locally.
     """
-    url = "https://fake-api-vycpfa6oca-uc.a.run.app/"
-    response = requests.post(url, json={"date": date})
+    url = "https://fake-api-vycpfa6oca-uc.a.run.app/sales"
+    response = requests.post(url, json={"date": date}, json={"page": page})
 
     if response.status_code != 200:
         raise Exception(f"API request failed: {response.status_code}, {response.text}")
 
     sales_data = response.json()
 
-    os.makedirs(raw_dir, exist_ok=True)
+    os.makedirs(page, exist_ok=True)
 
     # Save to file
-    file_path = os.path.join(raw_dir, f"sales_{date}.json")
+    file_path = os.path.join(page, f"sales_{date}.json")
     with open(file_path, "w", encoding="utf-8") as f:
         json.dump(sales_data, f, indent=2, ensure_ascii=False)
