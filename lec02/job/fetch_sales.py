f80ee236a61b193600c301107c6f607be699761b
Iryna <123470957+irinahnatenko@users.noreply.github.com>
2025-04-13 14:01:50 +0300

    Update fetch_sales.py

--- lec02/job/fetch_sales.py
+++ lec02/job/fetch_sales.py
@@ -13,22 +13,23 @@ def fetch_sales_data(page: str, date: str, raw_dir: str) -> None:
     headers = {"Authorization": f"Bearer {AUTH_TOKEN}"}
     params = {"date": date, "page": page}
 
+    response = requests.get(API_URL, params=params, headers=headers)
+    
+    if response.status_code == 200:
+        sales_data = response.json()
 
-response = requests.get(API_URL, params=params, headers=headers)
-if response.status_code == 200:
-    with open("C:/Users/User/robot_dreams_hw/lec02/job/raw/sales/sales_2022-08-09_1.json", "w", encoding="utf-8") as f:
-        json.dump(response.json(), f, indent=2, ensure_ascii=False)
-    print("✅ Данные сохранены!")
-else:
-    print(f"Ошибка API: {response.status_code}, {response.text}")
+        # Создание папки, если её нет
+        os.makedirs(raw_dir, exist_ok=True)
+        
+        # Сохранение данных в файл
+        file_path = os.path.join(raw_dir, f"sales_{date}_{page}.json")
+        with open(file_path, "w", encoding="utf-8") as f:
+            json.dump(sales_data, f, indent=2, ensure_ascii=False)
 
-    sales_data = response.json()
+        print(f"✅ Данные сохранены: {file_path}")
+    else:
+        print(f"❌ Ошибка API: {response.status_code}, {response.text}")
 
-    os.makedirs(raw_dir, exist_ok=True)
-    file_path = os.path.join(raw_dir, f"sales_{date}_{page}.json")
+fetch_sales_data("1", "2022-08-09", "C:/Users/User/robot_dreams_hw/lec02/job/raw/sales/")
 
-    with open(file_path, "w", encoding="utf-8") as f:
-        json.dump(sales_data, f, indent=2, ensure_ascii=False)
-
-    print(f"Данные сохранены: {file_path}")
 
